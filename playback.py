69ed30083f654b8989684a6bcba8e37f67e6617b
Aahil Lakhani <lakhaniaahil@gmail.com>
2022-04-01 00:53:10 -0400

    fix device args

--- playback.py
+++ playback.py
@@ -5,27 +5,43 @@ from gpiozero import LEDBoard
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-def update_indicator_leds(curr_plbk):
-    if curr_plbk['shuffle_state']:
+
+CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+REDIRECT_URI = 'https://localhost/riffplayer'
+SCOPE = 'user-read-playback-state,user-modify-playback-state'
+DEFAULT_DEVICE_ID = '0bb4d726656ae60024e260e346f6dedf33f2348d'
+
+toggle_playback_btn = Button(13)
+next_btn = Button(6)
+previous_btn = Button(19)
+shuffle_btn = Button(26)
+repeat_btn = Button(5)
+
+shuffle_led = LED(21)
+repeat_leds = LEDBoard(20, 16)
+
+def update_indicator_leds(shuffle_state, repeat_state):
+    if shuffle_state:
         shuffle_led.on()
     else:
         shuffle_led.off()
-    if curr_plbk['repeat_state'] == 'context':
+    if repeat_state == 'context':
         repeat_leds.value = (1, 0)
-    elif curr_plbk['repeat_state'] == 'track':
+    elif repeat_state == 'track':
         repeat_leds.value = (1, 1)
     else:
         repeat_leds.off()
 
-def toggle_playback(is_playing, curr_device_id):
-    if is_playing and curr_device_id == DEVICE_ID:
+def toggle_playback(is_playing, curr_device_id, target_device_id):
+    if is_playing and curr_device_id == target_device_id:
         sp.pause_playback()
         print("Playback paused")
-    elif curr_device_id == DEVICE_ID:
+    elif curr_device_id == target_device_id:
         sp.start_playback()
         print("Playback started")
     else:
-        sp.transfer_playback(DEVICE_ID)
+        sp.transfer_playback(target_device_id)
         print("Playback transferred")
 
 def toggle_shuffle(shuffle_state):
@@ -52,9 +68,9 @@ def cycle_repeat(repeat_state):
     elif repeat_state is None:
         print("Repeat not set: player inactive")
 
-def playback_control(curr_plbk):
+def playback_control(curr_plbk, target_device_id=DEFAULT_DEVICE_ID):
     if toggle_playback_btn.is_pressed:
-        toggle_playback(curr_plbk['is_playing'], curr_plbk['device']['id'])
+        toggle_playback(curr_plbk['is_playing'], curr_plbk['device']['id'], target_device_id)
     elif next_btn.is_pressed:
         sp.next_track()
         print("Skipped to next track")
@@ -66,21 +82,6 @@ def playback_control(curr_plbk):
     elif repeat_btn.is_pressed:
         cycle_repeat(curr_plbk['repeat_state'])
 
-CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
-CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
-REDIRECT_URI = 'https://localhost/riffplayer'
-SCOPE = 'user-read-playback-state,user-modify-playback-state'
-DEVICE_ID = '0bb4d726656ae60024e260e346f6dedf33f2348d'
-
-toggle_playback_btn = Button(13)
-next_btn = Button(6)
-previous_btn = Button(19)
-shuffle_btn = Button(26)
-repeat_btn = Button(5)
-
-shuffle_led = LED(21)
-repeat_leds = LEDBoard(20, 16)
-
 while True:
     try:
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=SCOPE))
@@ -91,7 +92,7 @@ while True:
                 # Assume these values
                 curr_plbk = {'is_playing': False, 'shuffle_state': None, 'repeat_state': None, 'device': {'id': None}}
 
-            update_indicator_leds(curr_plbk)
+            update_indicator_leds(curr_plbk['shuffle_state'], curr_plbk['repeat_state'])
             playback_control(curr_plbk)
     except Exception as e:
         print(e)
